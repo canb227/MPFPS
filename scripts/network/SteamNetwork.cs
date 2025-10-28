b5c462cf32426020a1aff8dc0ec6ce3fc14070e4
jeffercizer <jeffercize@gmail.com>
2025-10-28 13:06:54 -0400

    perframe message check

--- scripts/network/SteamNetwork.cs
+++ scripts/network/SteamNetwork.cs
@@ -54,7 +54,7 @@ public class SteamNetwork
     /// <summary>
     /// Max number of messages to attempt to process per frame. If we get frame delays because of spiky network traffic this needs turned down
     /// </summary>
-    private int maxMessagePerFramePerChannel = 100;
+    private int maxMessagePerFramePerChannel = 1000;
 
     /// <summary>
     /// if true, messages we send to ourself  get processed as if they had been sent over the network. If false, messages sent to ourself are discarded.
@@ -297,9 +297,12 @@ public class SteamNetwork
         {
             nint[] messages = new nint[maxMessagePerFramePerChannel];
             int messageCount = SteamNetworkingMessages.ReceiveMessagesOnChannel((int)channel, messages, maxMessagePerFramePerChannel);
-            for (int k = 0; k < messageCount; k++)
+            if(messageCount > 0)
             {
                 GD.Print($"We have {messageCount} messages incoming");
+            }
+            for (int k = 0; k < messageCount; k++)
+            {
                 if(channel == Channel.RPC)
                 {
                     GD.Print("Message on RPC");
