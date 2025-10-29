6e4dd15a664ee56f18bf355f1e2dc11822700fe6
jeffercizer <jeffercize@gmail.com>
2025-10-29 15:00:32 -0400

    crusher more logging

--- scripts/game/GameObjects/Triggerable/GOCrusher.cs
+++ scripts/game/GameObjects/Triggerable/GOCrusher.cs
@@ -25,10 +25,12 @@ public partial class GOCrusher : GOTrap
                 if (body is GOPackageBox box)
                 {
                     foundBox = box;
+                    Logging.Log("Found Box" + foundBox.orderNumber, "GOCrusher");
                 }
                 if (body is GOLabelPaper label)
                 {
                     foundLabel = label;
+                    Logging.Log("Found Label" + foundLabel.orderNumber, "GOCrusher");
                 }
             }
 
