34db65175d01fba40eeb736d09483d85f6a625f1
jeffercizer <jeffercize@gmail.com>
2026-02-27 10:44:55 -0500

    revert freeze change

--- scripts/game/GameObjects/RigidBodies/SimpleShape.cs
+++ scripts/game/GameObjects/RigidBodies/SimpleShape.cs
@@ -21,11 +21,8 @@ public partial class SimpleShape : GOBaseRigidBody, IsHoldable
         base._Ready();
         if (authority != Global.steamid)
         {
-            Freeze = true;
-            FreezeMode = FreezeModeEnum.Kinematic;
             SetPhysicsProcess(false);
         }
-
         priority = 10;
     }
     public override void ProcessStateUpdate(byte[] update)
